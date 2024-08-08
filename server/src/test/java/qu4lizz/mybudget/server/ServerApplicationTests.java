fffe9b7b6a7165826678c446c5b6d610e0af049e
qu4lizz <miroslavbratic00@gmail.com>
2024-08-08 18:14:58 +0200

    server init

---
+++ server/src/test/java/qu4lizz/mybudget/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package qu4lizz.mybudget.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
