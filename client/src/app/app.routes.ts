852e7aac00e5eb597816f23c3768e80192c3bebd
qu4lizz <miroslavbratic00@gmail.com>
2024-08-09 11:54:49 +0200

    client: header

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NavigationLayoutComponent } from './layouts/navigation-layout/navigation-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: NavigationLayoutComponent,
+    children: [],
+  },
+];
---
+++ client/src/app/components/shared/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MenubarModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Accounts',
+        icon: PrimeIcons.WALLET,
+        routerLink: '/',
+      },
+      {
+        label: 'Transactions',
+        icon: PrimeIcons.RECEIPT,
+        routerLink: '/transactions',
+      },
+      {
+        label: 'Settings',
+        icon: PrimeIcons.COG,
+        routerLink: '/settings',
+      },
+    ];
+  }
+}
