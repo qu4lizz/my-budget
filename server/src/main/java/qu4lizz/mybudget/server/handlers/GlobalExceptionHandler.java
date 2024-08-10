de2d743cb8378442f94e0810feb4a7d330aba21c
qu4lizz <miroslavbratic00@gmail.com>
2024-08-10 23:09:34 +0200

    server: exception handler

---
+++ server/src/main/java/qu4lizz/mybudget/server/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package qu4lizz.mybudget.server.handlers;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Object> handleBadRequestExceptions(BadRequestException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundExceptions(ChangeSetPersister.NotFoundException ex) {
+        return new ResponseEntity<>("Resource not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleException(Exception ex) {
+        ex.printStackTrace();
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
