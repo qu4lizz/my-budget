2a11da443661cf52f065ceb6ecb7f7c1c1266a89
qu4lizz <miroslavbratic00@gmail.com>
2024-08-10 15:39:35 +0200

    server: configuration

---
+++ server/src/main/java/qu4lizz/mybudget/server/config/AppConfig.java
@@ -0,0 +1,16 @@
+package qu4lizz.mybudget.server.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setAmbiguityIgnored(true);
+
+        return mapper;
+    }
+}
