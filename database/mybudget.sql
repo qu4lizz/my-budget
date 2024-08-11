1f121b12ea185b97f95b66007e2b3e6b25df4561
qu4lizz <miroslavbratic00@gmail.com>
2024-08-11 12:13:20 +0200

    database: settings table with init

--- database/mybudget.sql
+++ database/mybudget.sql
@@ -1,16 +1,3 @@
--- Database generated with pgModeler (PostgreSQL Database Modeler).
--- pgModeler version: 1.1.3
--- PostgreSQL version: 16.0
--- Project Site: pgmodeler.io
--- Model Author: ---
-
--- Database creation must be performed outside a multi lined SQL file. 
--- These commands were put in this file only as a convenience.
--- 
--- object: mybudget | type: DATABASE --
-
--- object: public.transaction | type: TABLE --
--- DROP TABLE IF EXISTS public.transaction CASCADE;
 CREATE TABLE public.transaction (
 	id serial NOT NULL,
 	description text NOT NULL,
@@ -19,12 +6,9 @@ CREATE TABLE public.transaction (
 	id_account integer NOT NULL,
 	CONSTRAINT transaction_pk PRIMARY KEY (id)
 );
--- ddl-end --
-ALTER TABLE public.transaction OWNER TO postgres;
--- ddl-end --
 
--- object: public.account | type: TABLE --
--- DROP TABLE IF EXISTS public.account CASCADE;
+ALTER TABLE public.transaction OWNER TO postgres;
+
 CREATE TABLE public.account (
 	id serial NOT NULL,
 	name varchar(40) NOT NULL,
@@ -32,15 +16,21 @@ CREATE TABLE public.account (
 	currency varchar(15) NOT NULL,
 	CONSTRAINT account_pk PRIMARY KEY (id)
 );
--- ddl-end --
-ALTER TABLE public.account OWNER TO postgres;
--- ddl-end --
 
--- object: account_fk | type: CONSTRAINT --
--- ALTER TABLE public.transaction DROP CONSTRAINT IF EXISTS account_fk CASCADE;
+ALTER TABLE public.account OWNER TO postgres;
+
 ALTER TABLE public.transaction ADD CONSTRAINT account_fk FOREIGN KEY (id_account)
 REFERENCES public.account (id) MATCH FULL
 ON DELETE RESTRICT ON UPDATE CASCADE;
--- ddl-end --
 
 
+CREATE TABLE settings (
+	id SERIAL PRIMARY KEY,
+	default_currency VARCHAR(15) NOT NULL,
+	migrated BOOLEAN NOT NULL,
+	UNIQUE(id)
+);
+
+ALTER TABLE public.settings OWNER TO postgres;
+
+INSERT INTO settings (default_currency, migrated) VALUES ('eur', false);
