0010bff12df669c92139233978f98a30e0d50945
qu4lizz <miroslavbratic00@gmail.com>
2024-08-12 22:39:57 +0200

    server: validators error handling

--- server/src/main/java/qu4lizz/mybudget/server/handlers/GlobalExceptionHandler.java
+++ server/src/main/java/qu4lizz/mybudget/server/handlers/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,6 +18,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
     public ResponseEntity<Object> handleNotFoundExceptions(ChangeSetPersister.NotFoundException ex) {
         return new ResponseEntity<>("Resource not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>(ex.getBody().getDetail(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
